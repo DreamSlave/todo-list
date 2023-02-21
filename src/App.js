205fe4ac581fab22b57e1c45414b15e6d329b698
sein0112 <tpdls973@naver.com>
2023-02-21 17:20:16 +0900

    feat : 메인화면 개발용 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.module.css';
-import styles from './App.module.css' //모듈 스타일 import
+import styles from './App.module.css'
+import First from "./First";
+import Task from "./Task";
+import Main from "./Main"; //모듈 스타일 import
 
 
 function App() {
   return (
       <div>
-        <div className={styles['frame0']}>
-          <div className={styles['group16']}>
-            <div className={styles['navi']}>
-            <span className={styles['text']}>
-              <span>꿈을 기록해보세요!</span>
-            </span>
-            </div>
-          </div>
-          <div className={styles['frame1']} className={styles['frame1']}>
-            <div className={styles['frame']}>
-              <div>
-              <span className={styles['text02']}>
-                <span>해야할 일을 입력해보세요.</span>
-              </span>
-              </div>
-            </div>
-            <div className={styles['frame2']}>
-            <span className={styles['text04']}>
-              <span>비고를 입력해보세요.</span>
-            </span>
-            </div>
-            <div className={styles['frame3']}>
-              <div className={styles['tag']}>
-              <span className={styles['text06']}>
-                <span>카테고리를 입력해보세요.</span>
-              </span>
-              </div>
-            </div>
-          </div>
-          <span className={styles['text08']}>
-          <span>첫번째 할 일을 기록해보세요!</span>
-        </span>
-          <div className={styles['empty']}>
-          <span className={styles['text10']}>
-            <span>입장하기</span>
-          </span>
-          </div>
-        </div>
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/first" element={<First />}></Route>
+            <Route path="/main" element={<Main />}></Route>
+            <Route path="/task" element={<Task />}></Route>
+          </Routes>
+            <Link to="/first">FirstLayout</Link>
+            <Link to="/main">MainLayout</Link>
+            <Link to="/task">Task</Link>
+        </BrowserRouter>
       </div>
   );
 }
