e3bd957bc54e603ce1512c467cd64d4076fb7e3b
sein0112 <tpdls973@naver.com>
2023-05-10 20:56:10 +0900

    feat : 노션 API 연동을 위한 프록시 서버 url 추가

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -7,7 +7,12 @@ const senyId = '112b1f0b22a64a50a19fc8aa6ec366f6'
 const mainDataBaseId = senyId
 
 const token = `secret_xv2JxNIKcE6jnTqKJcwlB49264ov3BftEBrpRc3raU6`
-const notionDomain = 'https://api.notion.com'
+/*
+* 임시 프록시 서버 url https://cors-anywhere.herokuapp.com/corsdemo
+* 해당 url에서 서버 활성화 하는 버튼 클릭후 사용 가능
+* !!! 반드시 개발용으로만 사용할 것 !!!
+* */
+const notionDomain = 'https://cors-anywhere.herokuapp.com/https://api.notion.com'
 export default {
     notionDomain,
     mainDataBaseId,
