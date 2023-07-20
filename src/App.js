4b830a16363d722ad4e60c2975ecc26543edbdb4
sein0112 <tpdls973@naver.com>
2023-07-20 18:42:27 +0900

    change : 라우터 분기처리 커밋누락 재커밋

--- src/App.js
+++ src/App.js
@@ -3,17 +3,13 @@ import './assets/css/App.module.css';
 import styles from './assets/css/App.module.css'
 // import Routes from "./route/Router.js";
 import ApiUtil from "./api/api.util.js";
-
 import {Outlet, RouterProvider} from 'react-router-dom';
-import Header from "./component/Header";
 import router from "./route/Router";
 ApiUtil.init()
 function App() {
   return (
       <div className={styles['frame0']}>
-          <React.StrictMode>
-              <RouterProvider router={router} />
-          </React.StrictMode>,
+          <RouterProvider router={router} />
       </div>
   );
 }
